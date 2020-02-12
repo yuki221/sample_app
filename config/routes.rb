75e915d330f0ba80549d21819b3c5f298f0c6307
chris <e1521411@gmail.com>
2020-02-12 19:32:57 +0900

    [add]指定のタグを持つ投稿のみを表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,26 @@ Rails.application.routes.draw do
   get 'spot_pages/night'
   get 'spot_pages/dinner'
   get 'users/new'
+
   devise_for :users
+
   get 'pages/index'
   get 'pages/show'
   get 'pages/home'
   get 'pages/help'
   get 'pages/about'
+
   get 'users/show'
   get 'users/index'
+
   get 'posts/new'
   get 'posts/index'
+
   get 'posts/night'
   get 'posts/dinner'
+  get 'posts/cafe'
+  get 'posts/sweets'
+
   get "posts/show/:id" => "posts#show"
   delete 'posts/show/:id' => 'posts#destroy'
   root "pages#home"
